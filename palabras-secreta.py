324bc06e002e0e855f0b56b27ec9cad4b7b4e8be
lilianaalisares <lili.alisares@gmail.com>
2025-05-21 16:07:04 -0300

    agregue comentarios

--- palabras-secreta.py
+++ palabras-secreta.py
@@ -1,13 +1,15 @@
+#se creará un juego, donde el usuario tiene que adivinar ciertas palabras colocando letras al azar. Se usará el modulo random.
 import random
 
 def juego_adivinar_palabra():
-    palabras = ["python", "programacion", "juego", "codigo"]
+    palabras = ["python", "programacion", "juego", "codigo"] #listas de palabras para el juego
     palabra_secreta = random.choice(palabras)
     letras_adivinadas = []
-    intentos = 6
+    intentos = 6 #números de intentos permitidos
 
     print("¡Bienvenido al juego de Adivina la palabra secreta!")
 
+    #bucle principal del juego
     while intentos > 0:
         mostrar = [letra if letra in letras_adivinadas else "_" for letra in palabra_secreta]
         print("Palabra:", " ".join(mostrar))
@@ -26,6 +28,7 @@ def juego_adivinar_palabra():
             letras_adivinadas.append(intento)
             print("¡Bien! La letra está en la palabra.")
         else:
+            #dismuir el número de intentos con el mensaje de "incorecto"
             intentos -= 1
             print("❌ Incorrecto. Intentos restantes:", intentos)
 
